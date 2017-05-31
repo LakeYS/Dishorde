234ee0668e6b555a84d1a1669bf368ac38fa509a
Lake <LakeYS@users.noreply.github.com>
2017-05-30 21:03:57 -0400

    Bot now relays chat messages from server, etc.
    - Messages from the server are now relayed to Discord.
    - Client now closes on process exit.

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ var telnet = require('telnet-client');
 connection = new telnet();
 
 ////// Arguments
-pass = process.argv[3]; // Simple temporary check.
+// TODO: Replace with proper checks
+pass = process.argv[3];
+token = process.argv[5];
+channelid = process.argv[7].toString();
 
 ////// Telnet
 var params = {
@@ -45,7 +48,25 @@ connection.on('close', function() {
 })
 
 connection.on('data', function(data) {
-  console.log(data.toString());
+  var data = data.toString();
+  var split = data.split(" ");
+  var type = split[3];
+
+  if(type == "Chat:")
+  {
+    // Make sure the channel exists.
+    if(channel !== null)
+    {
+      // Cut off the timestamp and other info
+      var msg = data.substring(40,data.length);
+
+      // Convert it to Discord-friendly text.
+      msg = msg.replace("'","").replace("'","");
+
+      channel.sendMessage(msg);
+      console.log(msg);
+    }
+  }
 });
 
 connection.on('error', function(data) {
@@ -54,10 +75,35 @@ connection.on('error', function(data) {
 
 connection.connect(params);
 
+////// # Discord
+client.login(token);
+
+client.on("message", msg => {
+
+});
+
+client.on('ready', () => {
+  console.log('Connected to ' + client.guilds.size + ' servers.');
+
+  channel = client.channels.find("id","319257907579453440");
+
+  console.log(channel);
+  console.log(channelid);
+});
+
+client.on('message', function(msg) {
+  if(msg.channel == channel && msg.author != client.user)
+    channel.sendMessage("WIP: Messages are not sent to the server yet.");
+});
+
 ////// # Input
 process.stdin.on('data', function (text) {
-  if(text.toString() == "stop\r\n")
+  if(text == "stop\r\n")
     process.exit();
   else
     eval(text.toString());
+});
+
+process.on('exit',  () => {
+  client.destroy();
 });
