60552ad10b966db42a54b95d21c8156353fe67e0
Lake <LakeYS@users.noreply.github.com>
2017-07-17 10:44:10 -0400

    Added experimental run_silent.vbs, misc. cleanup
    - Added run_silent.vbs, a simple VBS script that runs the bot in the
    background on Windows systems.
    - Added dependencies in package.json
    - Removed outdated information from run.bat and run.sh

--- run.bat
+++ run.bat
@@ -1,20 +1,4 @@
 @echo off
-rem WARNING: Keep this file secure as it contains sensitive information.
-
-rem See the official installation guide for more information:
-rem https://github.com/LakeYS/7DTD-Discord-Integration
-
-rem Note: Node.js and NPM are required to run this. Telnet must be enabled on your server.
-rem If any problems occur, ensure that both are installed and that your system is up-to-date.
-rem Support e-mail: contact@lakeys.net
-
-rem Port: (Optional) Your server's telnet port. This defaults to 8081 if none is specified.
-rem Password: Your server's telnet password.
-rem Token: Your Discord bot token.
-rem Channel: The channel you want the bot to operate in. (Will be optional in the future)
-
-rem Replace the following arguments with your Discord and game information.
-
 node ./index.js
 
 pause
--- run.sh
+++ run.sh
@@ -1,19 +1,3 @@
-# WARNING: Keep this file secure as it contains sensitive information.
-
-# See the official installation guide for more information:
-# https://github.com/LakeYS/7DTD-Discord-Integration
-
-# Note: Node.js and NPM are required to run this. Telnet must be enabled on your server.
-# If any problems occur, ensure that both are installed and that your system is up-to-date.
-# Support e-mail: contact@lakeys.net
-
-# Port: (Optional) Your server's telnet port. This defaults to 8081 if none is specified.
-# Password: Your server's telnet password.
-# Token: Your Discord bot token.
-# Channel: The channel you want the bot to operate in. (Will be optional in the future)
-
-# Replace the following arguments with your Discord and game information.
-
 node ./index.js
 
 read -p "Press enter to continue" nothing
