4e94b952aeff5902c20397ba1cc17621f78dd60d
Lake <contact@lakeys.net>
2017-12-30 15:01:37 -0500

    Auto-restart for run files, gitignore

--- run.bat
+++ run.bat
@@ -1,4 +1,13 @@
-@echo off
-node ./index.js
-
+@ECHO OFF
+node index.js
+:loop
+echo The bot will restart in 5 seconds. Press N to cancel.
+choice /t 5 /c yn /cs /d y /m "Start bot Y/N?"
+if errorlevel 3 goto :yes
+if errorlevel 2 goto :no
+if errorlevel 1 goto :yes
+:yes
+node index.js
+GOTO :loop
+:no
 pause
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-node ./index.js
-
-read -p "Press enter to continue" nothing
+until node index.js; do
+  echo "Application closed with exit code $?. Restarting in 5s, press Ctrl+C to cancel." >&2
+  sleep 5
+done
+read -p "Press enter to continue"
