0726878812686862daa0d62d5a859ee489c0ba3d
Lake <contact@lakeys.net>
2018-01-26 17:52:04 -0500

    ESLint and package scripts

---
+++ package_linux.sh
@@ -0,0 +1,7 @@
+mkdir 7DTD-Discord
+cp config.json 7DTD-Discord
+cp package.json 7DTD-Discord
+cp index.js 7DTD-Discord
+cp run.sh 7DTD-Discord
+cp README.md 7DTD-Discord
+tar -zcvf 7DTD-Discord.tar.gz 7DTD-Discord
---
+++ package_win.sh
@@ -0,0 +1,9 @@
+mkdir 7DTD-Discord
+cp config.json 7DTD-Discord
+cp package.json 7DTD-Discord
+cp index.js 7DTD-Discord
+cp install.bat 7DTD-Discord
+cp run.bat 7DTD-Discord
+cp profile.png 7DTD-Discord
+cp README.md 7DTD-Discord
+cp run_silent.vbs 7DTD-Discord
