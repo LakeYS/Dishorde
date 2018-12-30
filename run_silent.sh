ea7f732402ec2632720c446083979ae927cbe6f1
Lake <contact@lakeys.net>
2018-12-29 19:44:42 -0500

    Fix "dicord7dtd" (again)

--- run_silent.sh
+++ run_silent.sh
@@ -2,7 +2,7 @@
 # Auto-restart
 {
   echo "Launching... Use \"screen -r discord7dtd\" to access the terminal and ctrl+a+d to exit the terminal."
-  screen -dmS dicord7dtd ./run.sh
+  screen -dmS discord7dtd ./run.sh
 } || {
   read -p "Failed to launch, see log above. Please make sure that \"screen\" is installed on your system. Press enter to exit."
 }
